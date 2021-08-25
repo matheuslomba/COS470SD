ae71da2f196e7eb14aa7541b8cddf4a6b967f00e
Matheus Lomba <45018675+matheuslomba@users.noreply.github.com>
2021-08-25 09:22:54 -0300

    Update main.cpp

--- TP2/Semaforos/main.cpp
+++ TP2/Semaforos/main.cpp
@@ -7,62 +7,55 @@
 #include "stdlib.h"
 #include "unistd.h"
 #include <sys/types.h>
-#include <string>
+#include <string.h>
 #include <sys/wait.h>
 #include <time.h>
 #include <random>
 
-#include "../FuncSup/suporte.cpp"
+#include "aux"
+
 
 using namespace std;
 
-//Compilador:
-    //g++ somador.cpp -o run -pthread -std=c++11
-
-//-----------------------------------------------------------------------------------------------------
-
-//Variáveis Globais
 sem_t controlSemaphore;
 sem_t control_1_Semaphore;
 sem_t countSemaphore;
-queue <int> fila;
 
-//-----------------------------------------------------------------------------------------------------
+queue<int> fila;
+int ii;
 
-void Produtor(int tamanhoP) {
-
-    for (int i = 0; i < tamanhoP; i++) {
-        int r=ran107();
-
+void Produtor() {
+    
+    for(int i=0;i<1000;i++){
         //escrever numero na memoria 
         sem_wait(&countSemaphore);
+        
         sem_wait(&controlSemaphore);
-
+        int r=ran();
         fila.push(r);
-
         sem_post(&control_1_Semaphore);
+    
         sem_post(&controlSemaphore);
     }
 }
 
-void Consumidor(int tamanhoC) {
-
-    int ii = 0;
-
-    for (ii = 0; ii < tamanhoC; ii++) {
-
-        //ler na memoria
-        cout << ii << endl;
-        
+void Consumidor() {
+    //ler na memoria
+    
+    
+    while( ii<1000){
+    
         sem_wait(&control_1_Semaphore);
+
         sem_post(&countSemaphore);
-        sem_wait(&controlSemaphore);
         
-        int pr = fila.front();
+        sem_wait(&controlSemaphore);
+        int pr=fila.front();
         fila.pop();
-
+        cout<<"AQUI: " << ii;
+        
         bool verif = e_primo(pr);
-
+        ii++;
         if(verif==true){
             cout << "O número " << pr << " é primo!" << endl;
             sem_post(&controlSemaphore);
@@ -71,21 +64,23 @@ void Consumidor(int tamanhoC) {
             cout << "O número " << pr << " não é primo!" << endl;;
             sem_post(&controlSemaphore);
         }
-
     }
-
+    
+    exit(-1);
 }
+
 
 int main () {
 
-    FILE *rsemaforo = fopen("r_semaforo.csv", "w");
-    fprintf(rsemaforo,"N, Np, Nc, Tempo médio\n");
-
-    /*int N;
+    long tempo_inicial = time(NULL);
+    int N;
     printf("Defina o tamanho da fila: ");
     scanf("%d", &N);
-    sem_init(&countSemaphore, 2, N);
-    sem_init(&controlSemaphore, 0, 1);
+    sem_init(&countSemaphore, 1, N);
+    sem_init(&control_1_Semaphore, 1, 0);
+    sem_init(&controlSemaphore, 1, 1);
+    
+    cout << N << endl;;
 
     int Np;
     printf("Selecione a quantidade de threads produtoras a serem gerados: ");
@@ -93,64 +88,41 @@ int main () {
 
     int Nc;
     printf("Selecione a quantidade de threads consumidores a serem gerados: ");
-    scanf("%d", &Nc);*/
+    scanf("%d", &Nc);
     
-    int valN[5] = {1,2,4,16,32};
-    int valNpc[9][2] = {{1,1},{1,2},{1,4},{1,8},{1,16},{2,1},{4,1},{8,1},{16,1}};
+    std::thread threadsP[Np];
+    std::thread threadsC[Nc];
 
-    for (int nn = 0; nn < 5; nn++) {
-        
-        int N = valN[nn];
-        sem_init(&countSemaphore, 2, N);
-        sem_init(&controlSemaphore, 0, 1);
-        sem_init(&control_1_Semaphore, 1, 0);
-
-        for (int npc = 0; npc < 9; npc++) {
-            
-            int Np = valNpc[npc][0];
-            int Nc = valNpc[npc][1];
-
-            cout << N << "---------------------------------------------------" << endl;
-            cout << Np << "---------------------------------------------------" << endl;
-            cout << Nc << "---------------------------------------------------" << endl;
-
-            int tamanhoP = ceil(100/double(Np));
-            int tamanhoC = ceil(100/double(Nc));
-            std::thread threadsP[Np];
-            std::thread threadsC[Nc];
-
-            for (int m = 0; m < 10; m++) {
-                int tempo_medio = 0;
-                for (int tp = 0; tp < Np; tp++) {
-                    threadsP[tp] = std::thread(Produtor, tamanhoP);
-                }
-                for (int tc = 0; tc < Nc; tc++) {
-                    threadsC[tc] = std::thread(Consumidor, tamanhoC);
-                }
-
-                int tempo_inicial = clock();
-
-                for (int tp = 0; tp < Np; tp++) {
-                    threadsP[tp].join();
-                }
-                for (int tc = 0; tc < Nc; tc++) {
-                    threadsC[tc].join();
-                }
-
-                int tempo_final = clock();
-                tempo_medio += tempo_final - tempo_inicial;
-
-                //Envia os dados para um arquivo csv, para facilitar o estudo de caso 
-                fprintf(rsemaforo,"%d, %d, %d, %lf\n", N, Np, Nc, double(tempo_medio)/CLOCKS_PER_SEC);
-
-                cout << "Em N = " << N << ", com " << Np << " threads produtor e " << Nc << " threads consumidor, o programa foi executado em " << double(tempo_medio)/CLOCKS_PER_SEC << " segundos." << endl;
-
-                cout << "-------------------------------------------" << endl;
-            }
-
-        }
-
+    long tempo_medio = 0.000;
+    for (int tp = 0; tp < Np; tp++) {
+        threadsP[tp] = std::thread(Produtor);
+    }
+    for (int tc = 0; tc < Nc; tc++) {
+        threadsC[tc] = std::thread(Consumidor);
     }
     
+    
+    
+    for (int tp = 0; tp < Np; tp++) {
+        threadsP[tp].join();
+    }
+    for (int tc = 0; tc < Nc; tc++) {
+        threadsC[tc].join();
+    }
+
+    
+
+    //Envia os dados para um arquivo csv, para facilitar o estudo de caso 
+    //fprintf(rspinlock,"%d, %d, %d, %lf\n", n, k, somaTotal, double(tempo_medio)/CLOCKS_PER_SEC);
+
+    //cout << "Em K = " << k << " foi encontrado um somatório final de " << int(somaTotal) << ", executado em " << double(tempo_medio)/CLOCKS_PER_SEC << " segundos." << endl;
+    //somaTotal = 0;
+    
+    cout << "-------------------------------------------" << endl;
+    //}
+
+    long tempo_final = time(NULL);
+    tempo_medio = difftime(tempo_final,tempo_inicial);
+    cout<<"O tempo é: "<<tempo_medio;
     return 0;
 }
